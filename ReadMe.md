211f030422e15f1da450dd1d9864dd71dab4673c
David <davidisaacmijares@gmail.com>
2026-04-21 19:23:35 -0700

    Primer Commit

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# Plantilla-para-STM32f411
+Este es un repositorio con los archivos basicos para crear un proyecto usando una STM32f11 
---
+++ sources/main.c
@@ -0,0 +1,19 @@
+﻿/**
+ * @file main.c
+ * @brief 
+ *
+ * @authors David Mijares
+ *
+ */
+#include <stdint.h>
+
+
+int main()
+{
+            
+    while (1)
+    {
+
+    }
+    return 0; 
+}
